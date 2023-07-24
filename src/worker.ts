31290c24e0a06c8b877325597d1c0bb52ae88f54
ritikjaiswal75 <ritik12103032000@gmail.com>
2023-07-25 01:15:14 +0530

    add lint and pre-commit checks

--- src/worker.ts
+++ src/worker.ts
@@ -1,16 +1,7 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-import { env } from "./types/global.types";
+import { env } from './types/global.types';
 
 export default {
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	async fetch(request: Request, env: env, ctx: ExecutionContext): Promise<Response> {
 		return new Response('Hello World!');
 	},
