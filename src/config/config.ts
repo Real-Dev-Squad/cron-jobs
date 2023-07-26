b2bd88dadd94e40cbcfd70f468ddc0bc31fec21a
ritikjaiswal75 <ritik12103032000@gmail.com>
2023-07-27 00:24:39 +0530

    add strict type checking

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ import { env } from '../types/global.types';
 export const handleConfig = (env: env) => {
 	let baseUrl: string;
 	if (env.CURRENT_ENVIRONMENT) {
-		if (env.CURRENT_ENVIRONMENT == 'production') {
+		if (env.CURRENT_ENVIRONMENT.toLowerCase() === 'production') {
 			baseUrl = 'https://api.realdevsquad.com';
 		} else {
 			baseUrl = 'https://staging-api.realdevsquad.com';
