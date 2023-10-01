47859dea19086cde4984fb5195e0952b2886704e
Bharati Subramanian <bharatisharada@gmail.com>
2023-10-01 07:34:43 +0530

    refactor: scheduled handler type, return response

--- src/worker.ts
+++ src/worker.ts
@@ -5,14 +5,13 @@ export default {
 	// We need to keep all 3 parameters in this format even if they are not used as as cloudflare workers need them to be present So we are disabling eslint rule of no-unused-vars
 	// for more details read here: https://community.cloudflare.com/t/waituntil-is-not-a-function-when-using-workers-with-modules/375781/4
 	// eslint-disable-next-line no-unused-vars
-	async scheduled(req: Request, env: env, ctx: ExecutionContext) {
+	async scheduled(req: ScheduledController, env: env, ctx: ExecutionContext) {
 		switch (req.cron) {
 			case '0 */4 * * *':
 				ctx.waitUntil(ping(env));
 				break;
 			case '0 */6 * * *':
-				await callDiscordNicknameBatchUpdate(env);
-				break;
+				return await callDiscordNicknameBatchUpdate(env);
 			default:
 				console.log('Unknown Trigger Value!');
 		}
