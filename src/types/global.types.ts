fec88f7feb4964ed865b5ae77260657c2ee67cbd
Bharati Subramanian <bharatisharada@gmail.com>
2023-10-01 07:32:39 +0530

    feat: nickname batch update response type

--- src/types/global.types.ts
+++ src/types/global.types.ts
@@ -1,3 +1,12 @@
 export type env = {
 	[key: string]: string;
 };
+
+export type nicknameUpdateResponseType = {
+	message: string;
+	data: {
+		totalUsersStatus: number;
+		successfulNicknameUpdates: number;
+		unsuccessfulNicknameUpdates: number;
+	};
+};
