f054f7d5b68060630352f6242fb07a9b50b80e90
Prakash <om.pcprakash@gmail.com>
2023-11-04 01:38:34 +0530

    remove disabled eslint rule

--- src/worker.ts
+++ src/worker.ts
@@ -5,9 +5,6 @@ const EVERY_4_HOURS = '0 */4 * * *';
 const EVERY_6_HOURS = '0 */6 * * *';
 
 export default {
-	// We need to keep all 3 parameters in this format even if they are not used as as cloudflare workers need them to be present So we are disabling eslint rule of no-unused-vars
-	// for more details read here: https://community.cloudflare.com/t/waituntil-is-not-a-function-when-using-workers-with-modules/375781/4
-	// eslint-disable-next-line no-unused-vars
 	async scheduled(req: ScheduledController, env: env, ctx: ExecutionContext) {
 		switch (req.cron) {
 			case EVERY_4_HOURS:
