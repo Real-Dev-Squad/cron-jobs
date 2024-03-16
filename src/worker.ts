e70ad3a56a76b3980215dbf02c98fc1ad6b38fea
pavangbhat <pavanbhat17@gmail.com>
2024-03-16 19:03:31 +0530

    divided sync actions into two wrokers

--- src/worker.ts
+++ src/worker.ts
@@ -13,7 +13,8 @@ import { env } from './types/global.types';
 
 const EVERY_6_HOURS = '0 */6 * * *';
 const EVERY_11_HOURS = '0 */11 * * *';
-const EVERY_1_HOUR = '0 */1 * * *';
+const EVERY_20_MINUTES = '*/20 * * * *';
+const EVERY_30_MINUTES = '*/30 * * * *';
 
 export default {
 	// eslint-disable-next-line no-unused-vars
@@ -26,14 +27,20 @@ export default {
 				return await addMissedUpdatesRole(env);
 			}
 
-			case EVERY_1_HOUR: {
-				await syncUsersStatus(env);
-				await syncExternalAccounts(env);
-				await syncUnverifiedUsers(env);
+			case EVERY_20_MINUTES: {
 				await syncIdleUsers(env);
 				await syncNickNames(env);
 				await syncIdle7dUsers(env);
 				await syncOnboarding31dPlusUsers(env);
+				console.log('Worker for syncing idle users, nicknames, idle 7d users, and onboarding 31d+ users has completed.');
+				break;
+			}
+
+			case EVERY_30_MINUTES: {
+				await syncUsersStatus(env);
+				await syncExternalAccounts(env);
+				await syncUnverifiedUsers(env);
+				console.log('Worker for syncing user status, external accounts, and unverified users has completed.');
 				break;
 			}
 
